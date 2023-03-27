55e64abf6f4a8558a7f8e12c04999cfb1cfc51f1
“[MR <mizanurrahmanhridu@gmail.com>
2023-03-27 19:13:41 +0600

    Product List and Search Option Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React, { Fragment } from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import FullscreenLoader from "./components/masterLayout/Fullscreen-Loader";
+import ProductList from "./pages/Product-List";
 const App = () => {
     return (
-        <div>
-          
-        </div>
+        <Fragment>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<ProductList  />}  />}/>
+                </Routes>
+            </BrowserRouter>
+            <FullscreenLoader/>
+        </Fragment>
     );
 };
-
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import ReactDOM from 'react-dom';
+import "./assets/css/bootstrap.css";
+import "./assets/css/animate.min.css";
+import "./assets/css/style.css";
+import App from "./App";
+import {Provider} from "react-redux";
+import store from "./redux/store/store";
 
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-

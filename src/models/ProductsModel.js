c531fab11d3bf4b0525179eba3938a9a1c6ed692
“[MR <mizanurrahmanhridu@gmail.com>
2023-04-03 21:50:48 +0600

    new

--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -1,20 +1,28 @@
 const mongoose=require('mongoose');
 const DataSchema=mongoose.Schema(
     {
-        id:{type:Number},
-        title:{type:String},
-        price:{type:String},
-        special_price:{type:String},
-        image:{type:String},
-        category:{type:String},
-        subcategory:{type:String},
-        remark:{type:String},
-        brand:{type:String},
-        shop:{type:String},
-        shop_name:{type:String},
-        star:{type:String},
-        product_code:{type:String},
-        stock:{type:String}
+        // id:{type:Number},
+        // title:{type:String},
+        // price:{type:String},
+        // special_price:{type:String},
+        // image:{type:String},
+        // category:{type:String},
+        // subcategory:{type:String},
+        // remark:{type:String},
+        // brand:{type:String},
+        // shop:{type:String},
+        // shop_name:{type:String},
+        // star:{type:String},
+        // product_code:{type:String},
+        // stock:{type:String}
+
+            id: {type:String},
+            name: {type:String},
+            description: {type:String},
+            price: {type:String},
+            url: {type:String},
+            category: {type:String},
+            subcategory: {type:String}
     },
     {versionKey:false}
 );
